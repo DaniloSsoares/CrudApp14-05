45668b66f01a30ec6f3ce0db0817ab991d3fc62e
DaniloSsoares <122552017+DaniloSsoares@users.noreply.github.com>
2024-06-14 11:52:20 -0300

    Update Repository.kt
    Atualização

--- app/src/main/java/com/example/appatividadecrud/viewModel/Repository.kt
+++ app/src/main/java/com/example/appatividadecrud/viewModel/Repository.kt
@@ -2,12 +2,15 @@ package com.example.appatividadecrud.viewModel
 
 import com.example.appatividadecrud.RoomDB.Pessoa
 import com.example.appatividadecrud.RoomDB.PessoaDataBase
-//update e delete das class conectado ao banco de dados  atraves do recebmento do do PessoaDAO
+// Classe Repository que lida com as operações de atualização e deleção conectadas ao banco de dados através do PessoaDAO
 class Repository(private val db: PessoaDataBase) {
+
+class Repository(private val db: PessoaDataBase) {
+ // Função suspensa para inserir ou atualizar uma pessoa no banco de dados
     suspend fun upsertPessoa(pessoa: Pessoa){
         db.pessoaDao().upsertPessoa(pessoa)
     }
-
+// Função suspensa para deletar uma pessoa do banco de dados
     suspend fun deletePessoa(pessoa: Pessoa){
         db.pessoaDao().deletePessoa(pessoa)
     }
