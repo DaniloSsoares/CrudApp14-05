ebcd7614e547980e5589532497d18dbd736e1224
DaniloSsoares <122552017+DaniloSsoares@users.noreply.github.com>
2024-06-14 11:35:15 -0300

    Update PessoaViewModel.kt
    Atualização

--- app/src/main/java/com/example/appatividadecrud/viewModel/PessoaViewModel.kt
+++ app/src/main/java/com/example/appatividadecrud/viewModel/PessoaViewModel.kt
@@ -5,17 +5,22 @@ import androidx.lifecycle.asLiveData
 import com.example.appatividadecrud.RoomDB.Pessoa
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-//SERVE COMO FUNÇAO SQL NA TABELA
+// Declaração da classe PessoaViewModel, que herda de ViewModel
 class PessoaViewModel(private val repository: Repository): ViewModel() {
+    // Função para obter todas as pessoas do repositório e transformá-las em LiveData
     fun getPessoa() = repository.getAllPessoa().asLiveData(viewModelScope.coroutineContext)
 
+    // Função para inserir ou atualizar uma pessoa no repositório
     fun upsertPessoa(pessoa: Pessoa){
+         // Inicia uma coroutine no escopo do ViewModel
         viewModelScope.launch {
+            // Chama o método de inserir ou atualizar do repositório
             repository.upsertPessoa(pessoa)
         }
     }
-
+// Função para deletar uma pessoa do repositório
     fun deletePessoa(pessoa: Pessoa) {
+         // Inicia uma coroutine no escopo do ViewModel
         viewModelScope.launch {
             repository.deletePessoa(pessoa)
         }
