c86cf343b108e7ab290057de8ba4921d0561c32d
DaniloSsoares <122552017+DaniloSsoares@users.noreply.github.com>
2024-06-14 11:25:37 -0300

    Update Pessoa.kt
    Atualização

--- app/src/main/java/com/example/appatividadecrud/RoomDB/Pessoa.kt
+++ app/src/main/java/com/example/appatividadecrud/RoomDB/Pessoa.kt
@@ -3,12 +3,15 @@ package com.example.appatividadecrud.RoomDB
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-//Declaração das variaveis e seu formato de string para recebimento de infomações sendo definida como classe Pessoa
+// Declara a classe Pessoa como uma entidade do Room, representando uma tabela no banco de dados
 @Entity
 data class Pessoa(
+    // Declara o campo 'nome' do tipo String para armazenar o nome da pessoa
     val nome: String,
+    // Declara o campo 'telefone' do tipo String para armazenar o telefone da pessoa
     val telefone :String,
 
+    // Declara o campo 'id' como a chave primária da tabela, com geração automática de valores
     @PrimaryKey(autoGenerate =  true)
     val  id:Int = 0
 )
